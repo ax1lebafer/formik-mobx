29aaab59b24335089723e976b561208f537a0b26
ax1lebafer <raul8917@gmail.com>
2024-10-30 21:37:00 +0400

    Обновил компонент Button

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,15 +1,20 @@
 import styles from "./Button.module.css";
 import { ButtonHTMLAttributes } from "react";
 import * as React from "react";
+import cn from "classnames";
 
 type ButtonProps = ButtonHTMLAttributes<HTMLButtonElement> & {
   disabled?: boolean;
   children: React.ReactNode;
 };
 
-function Button({ disabled, children, ...props }: ButtonProps) {
+function Button({ disabled, children, className, ...props }: ButtonProps) {
   return (
-    <button disabled={disabled} className={styles.button} {...props}>
+    <button
+      disabled={disabled}
+      className={cn(styles.button, className)}
+      {...props}
+    >
       {disabled ? "Загрузка..." : children}
     </button>
   );
